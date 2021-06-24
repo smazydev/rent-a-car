2b5093372853b785123a30230a08f2b731c2c889
smazydev <alizafar1437@gmail.com>
2021-06-24 02:33:43 -0400

    Added responsiveness and styles for mobile links

--- src/app/components/navbar/navitems.tsx
+++ src/app/components/navbar/navitems.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
 import { slide as Menu } from "react-burger-menu";
 import { useMediaQuery } from "react-responsive";
@@ -12,7 +12,7 @@ const ListContainer = styled.ul`
 	list-none
 	`}
 `;
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
   ${tw`
 	text-xs
 	md:text-base
@@ -26,6 +26,16 @@ const NavItem = styled.li`
 	ease-in-out
 	hover:text-gray-700
 	`}
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+	text-white
+	text-xl
+	mb-3
+	focus:text-white
+	`}
+    `}
 `;
 
 export function NavItems() {
@@ -35,16 +45,16 @@ export function NavItems() {
     return (
       <Menu right styles={menuStyles}>
         <ListContainer>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Home</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Cars</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Services</a>
           </NavItem>
-          <NavItem>
+          <NavItem menu>
             <a href="#">Contact Us </a>
           </NavItem>
         </ListContainer>
