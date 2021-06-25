40f28912fd4010772f11e537d152588ccf27a3fa
smazydev <alizafar1437@gmail.com>
2021-06-25 01:22:25 -0400

    added booking calendar ui component

---
+++ src/app/components/marginer/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface IMarginProps {
+  margin: number | string;
+  direction?: "horizontal" | "vertical";
+}
+
+const HorizontalMargin = styled.span<IMarginProps>`
+  display: flex;
+  min-width: ${({ margin }) =>
+    typeof margin === "string" ? margin : `${margin}px`};
+`;
+const VerticalMargin = styled.span<IMarginProps>`
+  display: flex;
+  min-width: ${({ margin }) =>
+    typeof margin === "string" ? margin : `${margin}px`};
+`;
+
+function Marginer(props: IMarginProps) {
+  const { direction } = props;
+
+  if (direction === "horizontal") return <HorizontalMargin {...props} />;
+  else {
+    return <VerticalMargin {...props} />;
+  }
+}
+
+export { Marginer };
